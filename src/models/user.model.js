78a8c1bb6edd0ffd8845333922944bb275d54a0f
Satya Yalla <yallasatyamani@gmail.com>
2025-08-16 01:17:16 +0530

    changed generateaccesstoken and refresh token, mistake in spelling

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -61,7 +61,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
 
-userSchema.methods.genereateAccessToken = function (){
+userSchema.methods.generateAccessToken = function (){
     return jwt.sign(
         {
             _id: this._id, // we'll get this from mongodB
@@ -75,7 +75,7 @@ userSchema.methods.genereateAccessToken = function (){
         }
     )
 }
-userSchema.methods.genereateRefreshToken = function (){
+userSchema.methods.generateRefreshToken = function (){
     return jwt.sign(
         {
             _id: this._id, // we'll get this from mongodB
