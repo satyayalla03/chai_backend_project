5232988ccf93a58cd83e3f813fe164ef98cd2da2
Satya Yalla <yallasatyamani@gmail.com>
2025-08-16 00:40:07 +0530

    also added logout in route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import {Router} from "express"
-import { registerUser } from "../controllers/user.controllers.js"
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js"
 import { upload } from "../middlewares/multer.middleware.js";
+import { verify } from "jsonwebtoken";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
@@ -20,6 +22,11 @@ router.route("/register").post(
     registerUser
 )
 
+
+router.route("/login").post(loginUser)
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router
  
