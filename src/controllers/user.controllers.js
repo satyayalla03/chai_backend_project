063c8a99d6fa5f2a02e518329c34b1480f72f0b0
Satya Yalla <yallasatyamani@gmail.com>
2025-08-19 16:33:00 +0530

    fix: update getCurrentUser response to use ApiResponse and ensure proper async handling in updateAccountDetails

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -292,7 +292,9 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
 const getCurrentUser = asyncHandler(async(req, res) => {
     return res.status(200)
-    .json(200, req.user, "current user fetched succesfully")
+    .json(
+        new ApiResponse(200, req.user, "current user fetched succesfully")
+    )
 })
 
 const updateAccountDetails = asyncHandler(async(req, res) => {
@@ -302,7 +304,7 @@ const updateAccountDetails = asyncHandler(async(req, res) => {
         throw new ApiError(400, "all fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
